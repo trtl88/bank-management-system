1acd9da774f2c81b113308a8f87fc1e2d885cd34
trtl88 <154389843+trtl88@users.noreply.github.com>
2023-12-20 21:38:01 +0200

    Report.c

--- Report.c
+++ Report.c
@@ -1,18 +1,44 @@
 void Report()
 {
-    char acc[20],current[20];
-    int flag;
+    char acc[15],current[20];
     FILE *f;
-    printf("Enter account number to view report: ");
-    do
+    printf("Enter account number to view last 5 transactions made: ");
+    scanf("%s",acc);
+    strcat(acc,".txt");
+    while((strlen(acc)!=14)||(f=fopen(acc,"r"))==NULL)
     {
-        scanf("%10s",acc);
-        strcat(acc, ".txt");
-        if((f=fopen(acc,"r"))==NULL)
+        if(strlen(acc)!=14)
+        {
+            printf("Invalid account number, try again: ");
+            scanf("%s",acc);
+            strcat(acc, ".txt");
+        }
+        else
         {
             printf("Account number not found, try again: ");
-            flag=0;
+            scanf("%s",acc);
+            strcat(acc, ".txt");
         }
-        else flag=1;
     }
-    while(!flag);
+    printf("Last 5 transactions: ");
+    fseek(f,-2,SEEK_END);
+    int n=0;
+    char amount[10],c;
+    while (n<6)
+    {
+        int counter=-3;
+        c=fgetc(f);
+        while(c!='\n')
+          {
+            if(fseek(f,-2,SEEK_CUR)!=0)
+                break;
+            counter--;
+            c=fgetc(f);
+          }
+        fscanf(f,"%s",amount);
+        printf("%s\n",amount);
+        n++;
+        fseek(f,counter,SEEK_CUR);
+    }
+    fclose(f);
+}
