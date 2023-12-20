3e7f4f9552f31310e71ab356fa229c1fa19c50ff
trtl88 <154389843+trtl88@users.noreply.github.com>
2023-12-20 22:38:58 +0200

    Create Reportlessthan5.c

---
+++ Reportlessthan5.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+
+void Report()
+{
+    char acc[15],current[20];
+    FILE *f;
+    printf("Enter account number to view last 5 transactions made: ");
+    scanf("%s",acc);
+    strcat(acc,".txt");
+    while((strlen(acc)!=14)||(f=fopen(acc,"r"))==NULL)
+    {
+        if(strlen(acc)!=14)
+        {
+            printf("Invalid account number, try again: ");
+            scanf("%s",acc);
+            strcat(acc, ".txt");
+        }
+        else
+        {
+            printf("Account number not found, try again: ");
+            scanf("%s",acc);
+            strcat(acc, ".txt");
+        }
+    }
+    printf("Last 5 transactions: ");
+    fseek(f,-2,SEEK_END);
+    char amount[10],c;
+    int n=0;
+    while(n<6)
+    {
+        int counter=-3;
+        c=fgetc(f);
+        while(c!='\n')
+        {
+            for(int i=0; i<2; i++)
+            {
+                fseek(f,-1,SEEK_CUR);
+                if(ftell(f)<1)
+                {
+                    fseek(f,0,SEEK_SET);
+                    fscanf(f,"%s",amount);
+                    printf("%s\n",amount);
+                    return;
+                }
+            }
+            counter--;
+            c=fgetc(f);
+        }
+        fscanf(f,"%s",amount);
+        printf("%s\n",amount);
+        fseek(f,counter,SEEK_CUR);
+        n++;
+    }
+    fclose(f);
+}
