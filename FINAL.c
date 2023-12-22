f5c159fe325a047e1d919dd9c1cc6e72f78d53a7
trtl88 <154389843+trtl88@users.noreply.github.com>
2023-12-22 04:29:01 +0200

    FINAL.c

--- FINAL.c
+++ FINAL.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 #include <strings.h>
 
+void LOGIN();
+void LOAD();
+void SEARCH();
+void ADVANCED();
+void ADD();
+void DELETE();
+void MODIFY();
+void WITHDRAW();
+void DEPOSIT();
+void TRANSFER();
+void REPORT();
+void PRINT();
+void SAVE();
+void QUIT();
+void MENU();
+void SortByName();
+void SortByBalance();
+void SortByDate();
+
 typedef struct
 {
     int month;
@@ -25,116 +44,29 @@ typedef struct
 } log;
 
 account a[100]; //Global variable
-int count1=0; //index to know how many lines in accounts file
+int AccCount=0; //index to know how many lines in accounts file
+const char* MonthName[]={"January","February","March","April","May","June","July","August","September","October","November","December"};
 
-void report()
+int main()
 {
-    char acc[15],current[20];
-    FILE *f;
-    printf("Enter account number to view last 5 transactions made: ");
-    scanf("%s",acc);
-    strcat(acc,".txt");
-    while((strlen(acc)!=14)||(f=fopen(acc,"r"))==NULL)
-    {
-        if(strlen(acc)!=14)
-        {
-            printf("Invalid account number, try again: ");
-            scanf("%s",acc);
-            strcat(acc, ".txt");
-        }
-        else
-        {
-            printf("Account number not found, try again: ");
-            scanf("%s",acc);
-            strcat(acc, ".txt");
-        }
-    }
-    printf("Last 5 transactions: \n");
-    fseek(f,-3,SEEK_END); //sets position of pointer right before the last number in the file
-    char amount[10];
-    int n=0;
-    while(n<5) //prints 5 transactions or less
-    {
-        int counter=-3;
-        char c;
-        while ((c=fgetc(f))!='\n') //checks if the pointer is at a new line
-        {
-            fseek(f,-2,SEEK_CUR);
-            if(ftell(f)<2) //checks if this is the first transaction in the file, prints it and exits function
-            {
-                fseek(f,0,SEEK_SET); //resets pointer to beginning of file
-                fscanf(f,"%s",amount);
-                printf("%s\n",amount);
-                fclose(f);
-                menu();
-                return;
-            }
-            counter--;
-        }
-        n++;
-        fscanf(f,"%s",amount);
-        printf("%s\n",amount);//prints the transaction
-        fseek(f,counter,SEEK_CUR); //sets the pointer right before the first digit of the line above the one printed
-    } //loop repeats for the line above the printed one
+    //MENU();
+    PRINT();
+    /*int d;
+    FILE*f=fopen("1234567890.txt","w");
+    fprintf(f,"+100\n-2\n-1\n+1\n-2000\n+10000\n");
     fclose(f);
-    menu();
+    printf("choose option: ");
+    scanf("%d",&d);
+    if(d==1)
+        report();
+    if(d==2)
+        print();
+    if(d==3)
+        quit();*/
+    return 0;
 }
 
-void print()
-{
-
-}
-
-void menu()
-{
-    printf("1:ADD\n2:DELETE\n3:MODIFY\n4:SEARCH\n5:ADVANCED SEARCH\n6:WITHDRAW\n7:DEPOSIT\n8:TRANSFER\n9:REPORT\n10:PRINT\n11:QUIT\n");
-    printf("Choose one from the above options:\n");
-    int x;
-    scanf("%d", &x);
-    while(x<=0 || x>=12)
-    {
-        printf("Invalid. Enter a number between 1 and 11:\n");
-        scanf("%d", &x);
-    }
-    switch(x)
-    {
-    case 1:
-        printf("a");//call add func.
-        break;
-    case 2:
-        printf("b"); //call delete func.
-        break;
-    case 3:
-        printf("c"); //call modify func.
-        break;
-    case 4:
-        printf("d"); //call search func.
-        break;
-    case 5:
-        printf("e"); //call advanced search func.
-        break;
-    case 6:
-        printf("f"); //call withdraw func.
-        break;
-    case 7:
-        printf("h"); //call deposit func.
-        break;
-    case 8:
-        printf("i"); //call transfer func.
-        break;
-    case 9:
-        report(); //call report func.
-        break;
-    case 10:
-        printf("k"); //call print func.
-        break;
-    case 11:
-        quit(); //call quit func.
-        break;
-    }
-}
-
-void load()
+void LOAD()
 {
     int i=0;
     char c, x[500], temp[10];
@@ -175,19 +107,15 @@ void load()
             j++;
         }
         i++;
-        count1++;
+        AccCount++;
     }
+    fclose(f2);
 }
 
-void quit()
-{
-    exit(0);
-}
-
-int login()
+void LOGIN()
 {
     char username[100], password[100];
-    int i=0,m,flag=0;
+    int i=0,m;
     log s[100];
     FILE *f1=fopen("users.txt","r");
     while (!feof(f1))
@@ -196,45 +124,189 @@ int login()
         i++;
     }
     m=i;
-    while(1)
+    printf("Username: ");
+    scanf("%s",username);
+    printf("Password: ");
+    scanf("%s",password);
+    i=0;
+    while(i<m)
     {
-        printf("Username: ");
-        scanf("%s",username);
-        printf("Password: ");
-        scanf("%s",password);
-        i=0;
-        while(i<m)
+        if((strcmp(username,s[i].username))==0&&(strcmp(password,s[i].password))==0)
         {
-            if((strcmp(username,s[i].username))==0&&(strcmp(password,s[i].password))==0)
+            printf("\nWelcome %s!\n",username);
+            fclose(f1);
+            LOAD();
+            return;
+        }
+        i++;
+    }
+    printf("Wrong username or password.\n\n");
+    MENU();
+}
+
+void REPORT()
+{
+    char acc[15],current[20];
+    FILE *f;
+    printf("\nEnter account number to view last 5 transactions made: ");
+    scanf("%s",acc);
+    strcat(acc,".txt");
+    while((strlen(acc)!=14)||(f=fopen(acc,"r"))==NULL)
+    {
+        if(strlen(acc)!=14)
+        {
+            printf("Invalid account number, try again: ");
+            scanf("%s",acc);
+            strcat(acc, ".txt");
+        }
+        else
+        {
+            printf("Account number not found, try again: ");
+            scanf("%s",acc);
+            strcat(acc, ".txt");
+        }
+    }
+    printf("Last 5 transactions(newest to oldest): \n");
+    fseek(f,-3,SEEK_END); //sets position of pointer right before the last number in the file
+    char amount[10];
+    int n=0;
+    while(n<5) //prints 5 transactions or less
+    {
+        int counter=-3;
+        char c;
+        while ((c=fgetc(f))!='\n') //checks if the pointer is at a new line
+        {
+            fseek(f,-2,SEEK_CUR);
+            if(ftell(f)<2) //checks if this is the first transaction in the file, prints it and exits function
             {
-                printf("Welcome %s!\n",username);
-                flag=1;
-                menu();
+                fseek(f,0,SEEK_SET); //resets pointer to beginning of file
+                fscanf(f,"%s",amount);
+                printf("%s\n\n",amount);
+                fclose(f);
+                MENU();
                 return;
             }
-            i++;
+            counter--;
         }
-        flag=0;
-        printf("Wrong username or password, try again.\n");
-        fclose(f1);
+        n++;
+        fscanf(f,"%s",amount);
+        printf("%s\n",amount);//prints the transaction
+        fseek(f,counter,SEEK_CUR); //sets the pointer right before the first digit of the line above the one printed
+    } //loop repeats for the line above the printed one
+    printf("\n");
+    fclose(f);
+    MENU();
+}
+
+void PRINT()
+{
+    int z;
+    printf("Choose way of sorting:\n1.Name\n2.Balance\n3.Date opened\n");
+    scanf("%d",&z);
+    while(z!=1&&z!=2&&z!=3)
+    {
+        printf("Invalid. Enter 1, 2 or 3:\n");
+        scanf("%d",&z);
+    }
+    switch(z)
+    {
+    case 1:
+        SortByName();
+        break;
+    case 2:
+        SortByBalance();
+        break;
+    case 3:
+        SortByDate();
+        break;
+    }
+    int i;
+    for(i=0;i<AccCount;i++)
+    {
+        printf("Account Number: %s\n",a[i].AccNo);
+        printf("Name: %s\n",a[i].Name);
+        printf("E-mail: %s\n",a[i].Name);
+        printf("Balance: %s\n",a[i].Name);
+        printf("Mobile: %s\n",a[i].Name);
+        printf("Date Opened: %s %d\n",MonthName[a[i].DateOpened.month-1],a[i].DateOpened.year);
     }
 }
 
-int main()
+void SortByName()
 {
-    login();
-    //menu();
-    /*int d;
-    FILE*f=fopen("1234567890.txt","w");
-    fprintf(f,"+100\n-2\n-1\n+1\n-2000\n+10000\n");
-    fclose(f);
-    printf("choose option: ");
-    scanf("%d",&d);
-    if(d==1)
-        report();
-    if(d==2)
-        print();
-    if(d==3)
-        quit();*/
-    return 0;
+
+}
+void SortByBalance()
+{
+
+}
+void SortByDate()
+{
+
+}
+
+void QUIT()
+{
+    exit(0);
+}
+
+void MENU()
+{
+    printf("1.LOGIN\n2.QUIT\n");
+    printf("Choose one from the above options:\n");
+    int z;
+    scanf("%d",&z);
+    while(z!=1&&z!=2)
+    {
+        printf("Invalid. Enter 1 or 2:\n");
+        scanf("%d",&z);
+    }
+    if(z==1)
+        LOGIN();
+    else QUIT();
+    printf("1:ADD\n2:DELETE\n3:MODIFY\n4:SEARCH\n5:ADVANCED SEARCH\n6:WITHDRAW\n7:DEPOSIT\n8:TRANSFER\n9:REPORT\n10:PRINT\n11:QUIT\n");
+    printf("Choose one from the above options:\n");
+    int x;
+    scanf("%d",&x);
+    while(x<=0 || x>=12)
+    {
+        printf("Invalid. Enter a number between 1 and 11:\n");
+        scanf("%d",&x);
+    }
+    switch(x)
+    {
+    case 1:
+        printf("a");//call add func.
+        break;
+    case 2:
+        printf("b"); //call delete func.
+        break;
+    case 3:
+        printf("c"); //call modify func.
+        break;
+    case 4:
+        printf("d"); //call search func.
+        break;
+    case 5:
+        printf("e"); //call advanced search func.
+        break;
+    case 6:
+        printf("f"); //call withdraw func.
+        break;
+    case 7:
+        printf("h"); //call deposit func.
+        break;
+    case 8:
+        printf("i"); //call transfer func.
+        break;
+    case 9:
+        REPORT(); //call report func.
+        break;
+    case 10:
+        PRINT(); //call print func.
+        break;
+    case 11:
+        QUIT(); //call quit func.
+        break;
+    }
 }
